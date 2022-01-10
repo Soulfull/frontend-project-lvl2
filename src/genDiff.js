dd7e3bf10e182b4da2f316c7ecb5771515df03c1
m.smolin <m.smolin@space307.com>
2022-01-10 23:47:52 +0300

    add test

--- src/genDiff.js
+++ src/genDiff.js
@@ -7,7 +7,8 @@ const genDiff = (path1, path2) => {
   const data1 = fs.readFileSync(path.resolve(path1), 'utf8');
   const data2 = fs.readFileSync(path.resolve(path2), 'utf8');
   const diff = compare(JSON.parse(data1), JSON.parse(data2));
-  return console.log(toStringObject(diff));
+  const str = toStringObject(diff);
+  return console.log(str);
 };
 
 export default genDiff;
